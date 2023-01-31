383e316d962aa14200bd7faee05691ab0cfc1c05
Shubhangi Chauhan <Shubhangi Chauhan@LAPTOP-P6VV32JO>
2023-01-31 20:40:58 +0530

    Add project files.

---
+++ Models/ViewImage.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+
+namespace Assignment4.Models
+{
+    public class ViewImage
+    {
+        public ObjectId Id { get; set; }
+        public string url { get; set; }
+        public string Description { get; set; }
+
+    }
+}
